1cfca3852276acf0d73528fae8414c6828c7eaf3
Arsh Sekhon <arshs3khon@gmail.com>
2021-02-17 13:43:25 -0800

    Add setup.py for package

---
+++ seqnereval/__version__.py
@@ -0,0 +1,14 @@
+
+"""
+Variables for setup.py are defined here, allowing them to be accessed from
+within the package:
+Example:
+    >>> from pubtator_loader.__version__ import __version__
+"""
+__name__ = "seqnereval"
+__version__ = "0.1.0"
+__description__ = "Parse PubTator files with ease"
+__url__ = "https://github.com/arshsekhon/seqnereval"
+__author__ = "Arsh Sekhon"
+__author_email__ = "arshs3khon@gmail.com"
+__license__ = "GNU GPL v3"
